77e05e1f340d09c1d155bb6cc77e2c7bb230e753
Ishan Aggarwal <47055257+ishanaggarwal@users.noreply.github.com>
2024-07-30 00:33:02 -0400

    Delete Hello.js

--- Hello.js
+++
@@ -1,9 +0,0 @@
-export default function Hello(app) {
-    app.get('/hello', (req, res) => {
-      res.send('Life goes On!')
-    })
-    app.get('/', (req, res) => {
-      res.send('Welcome to Full Stack Development!')
-    })
-  }
-  
